49ee25719530dafab4b7774493d405ec660ecb82
Romain Durade <durade.romain@gmail.com>
2018-04-13 15:12:20 +0200

    implement barman

--- src/fr/epsi/binome/Barman.java
+++ src/fr/epsi/binome/Barman.java
@@ -1,14 +1,39 @@
 package fr.epsi.binome;
 
 public class Barman extends Thread {
+	
+	private String name;
+	
+	private Backlog orders;
 
 	public Barman(String name, Backlog orders) {
-		// TODO Auto-generated constructor stub
+		super();
+		this.name = name;
+		this.orders = orders;
 	}
 
+
+
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() {		
+		Cocktail cCocktail;
+        do {
+
+            cCocktail= orders.dequeue();
+            
+            if(cCocktail!=null) {
+            	String message = String.format("%s as prepared  %s", this.name, cCocktail);
+                System.out.println(this.name + " "+ cCocktail);
+                
+            }
+           
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("ERR --> "+e.getMessage());
+            }
+            
+        } while (!orders.isEmpty());
 
 	}
 
